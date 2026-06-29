819047fb4357ac9689647de07b28b7bee2bce1e2
Manas Ranjan Bera <manasbera484@gmail.com>
2026-06-29 16:19:54 +0530

    js

---
+++ JS/Class 1/script.js
@@ -0,0 +1,58 @@
+// console.log("Hello everyone, good afternoon");
+
+//! Variable Keywords
+//? var
+// var a = 10
+// console.log(a);
+// var a = 20
+// console.log(a);
+// a = "Hi"
+// console.log(a);
+
+// var x = 10
+// console.log(x);
+// x = 0
+// console.log(x);
+
+//? let
+// let b = 10
+// console.log(b);
+// let b = 20 //error
+b = "Hello"
+// console.log(b);
+
+//? const
+// const c = 10
+// c = 20
+// console.log(c);
+
+//! Data Types
+//* Primitive Types
+//? Number type
+let n = 10
+let n2 = 10.2
+let n3 = -10.5
+// console.log(typeof n);
+// console.log(typeof n2);
+// console.log(typeof n3);
+
+//? Boolean
+b1 = true
+b2 = false
+// console.log(typeof b1, typeof b2);
+
+//? String
+let str = 'Hello'
+let str2 = '1234568'
+let str3 = "!@#$%&*"
+// console.log(typeof str);
+// console.log(typeof str2);
+// console.log(typeof str3);
+
+//? Undefined
+let x
+// console.log(typeof x);
+
+//? Null
+let a = null
+// console.log(typeof a);
